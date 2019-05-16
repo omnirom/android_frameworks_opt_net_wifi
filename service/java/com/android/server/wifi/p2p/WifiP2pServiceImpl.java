fef0d2685f21e5fe4b8260d4b08e9dae5069ce5b
Purushottam Kushwaha <pkushwah@codeaurora.org>
2019-05-16 12:20:20 +0530

    P2P: Avoid P2P initialization after continuous setup failures.
    P2P initialization may fail in supplicant (ex: when no social
    channels are available). This failure is reported back to P2P service
    and p2p service initiate cleanup. As part of this cleanup, Interface
    Destroyed and Interface Available listeners are triggered from
    HalDeviceManager. P2P service on receiving Interface Availability,
    triggers p2p setup. This ends up in an infinite loop.
    
    This commit counts such continuous setup failures with a threshold
    of P2P_SETUP_FAILURE_COUNT_THRESHOLD (currently set to 10) and there
    after ignores Interface Availability callbacks. This count is reset
    to 0 on subsequent Wifi on/off or on a successful p2p initialization.
    Thus, avoids infinite attempts of p2p setup failures.
    
    Note: This change doesn't impact user/Application P2P on/off.
    
    Change-Id: I83edce5b94fcd003dfc3faf0788c05c669280485
    CRs-Fixed: 2453826

--- service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
+++ service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
@@ -783,6 +783,13 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
         // Is wifi on or off.
         private boolean mIsWifiEnabled = false;
 
+        // p2p start failure count.
+        // Current design keeps on calling setupIface even though p2p init fails in supplicant.
+        // Break this infinite loop by maintaining a temp counter for continuous setup failures.
+        private int mSetupFailureCount = 0;
+        // Threshold for continuous setup failure.
+        private static final int P2P_SETUP_FAILURE_COUNT_THRESHOLD = 10;
+
         // Saved WifiP2pConfig for an ongoing peer connection. This will never be null.
         // The deviceAddress will be an empty string when the device is inactive
         // or if it is connected without any ongoing join request
@@ -857,6 +864,7 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
                             checkAndReEnableP2p();
                         } else {
                             mIsWifiEnabled = false;
+                            mSetupFailureCount = 0;
                             // Teardown P2P if it's up already.
                             sendMessage(DISABLE_P2P);
                         }
@@ -878,10 +886,14 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
                 // Register for interface availability from HalDeviceManager
                 mWifiNative.registerInterfaceAvailableListener((boolean isAvailable) -> {
                     mIsHalInterfaceAvailable = isAvailable;
-                    if (isAvailable) {
-                        checkAndReEnableP2p();
+                    if (mSetupFailureCount < P2P_SETUP_FAILURE_COUNT_THRESHOLD) {
+                        if (isAvailable) {
+                            checkAndReEnableP2p();
+                        }
+                        checkAndSendP2pStateChangedBroadcast();
+                    } else {
+                        Log.i(TAG, "Ignore InterfaceAvailable for continuous failures. count=" +mSetupFailureCount);
                     }
-                    checkAndSendP2pStateChangedBroadcast();
                 }, getHandler());
 
                 mFrameworkFacade.registerContentObserver(mContext,
@@ -1434,9 +1446,11 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
                             checkAndSendP2pStateChangedBroadcast();
                         }, getHandler());
                         if (mInterfaceName == null) {
+                            mSetupFailureCount++;
                             Log.e(TAG, "Failed to setup interface for P2P");
                             break;
                         }
+                        mSetupFailureCount = 0;
                         setupInterfaceFeatures(mInterfaceName);
                         try {
                             mNwService.setInterfaceUp(mInterfaceName);
