fe60e68ff41d342d9af1cdbb13b0aa1e0beaf4ee
Hu Wang <huw@codeaurora.org>
2021-01-09 11:24:10 +0800

    wifi: Support ConfigStore file recovery if XML parse fail
    If ConfigStore file is empty or corrupted, XML parse will raise
    Exception. This results in:
    1) All the configuration will lost, and
    2) wifi can't connect to any networks with error message:
    "WifiConfigManager: Cannot add/update network before store is read"
    
    This CL supports ConfigStore file recovery if XML parse fail,
    which address issue 2).
    
    CRs-Fixed: 2849026
    Change-Id: I98152d19d359857c4471beead68d75fb9b5354a1

--- service/java/com/android/server/wifi/WifiConfigManager.java
+++ service/java/com/android/server/wifi/WifiConfigManager.java
@@ -3107,10 +3107,8 @@ public class WifiConfigManager {
         } catch (IOException | IllegalStateException e) {
             Log.wtf(TAG, "Reading from new store failed. All saved networks are lost!", e);
             return false;
-        } catch (XmlPullParserException e) {
-            Log.wtf(TAG, "XML deserialization of store failed. All saved networks are lost!", e);
-            return false;
-        }
+        } // XmlPullParserException is handled by config store file itself.
+
         loadInternalData(mNetworkListSharedStoreData.getConfigurations(),
                 mNetworkListUserStoreData.getConfigurations(),
                 mRandomizedMacStoreData.getMacMapping());
@@ -3142,11 +3140,8 @@ public class WifiConfigManager {
         } catch (IOException | IllegalStateException e) {
             Log.wtf(TAG, "Reading from new store failed. All saved private networks are lost!", e);
             return false;
-        } catch (XmlPullParserException e) {
-            Log.wtf(TAG, "XML deserialization of store failed. All saved private networks are "
-                    + "lost!", e);
-            return false;
-        }
+        } // XmlPullParserException is handled by config store file itself.
+
         loadInternalDataFromUserStore(mNetworkListUserStoreData.getConfigurations());
         return true;
     }
